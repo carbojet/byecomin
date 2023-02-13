4a2fbdeec84a927a866aa0353810cf49df1898c6
Sabyasachi Rout <sabyasachirout@byecom.in>
2023-02-13 11:53:23 +0530

    mobile responsive header fixed

--- components/header.js
+++ components/header.js
@@ -24,60 +24,83 @@ export default function Header() {
   return (
     <>
       <div
-            data-collapse='medium'
-            data-animation='default'
-            data-duration='400'
-            data-w-id='0b6d58b7-c8f6-c4c2-5d56-04a30b2a6532'
-            role='banner'
-            className='navigation no-color w-nav'
-        >
-            <div className='container grid w-container'>
-                <div id='w-node-04a30b2a6534-0b2a6532' className='flex-center'>
-                    <Link
-                        passHref
-                        href='/'
-                        className='brand left-margin w-nav-brand'
-                    >
-                        <Image width={150} height={45}  alt="byecom logo" src="/byecom-logo.png" />
-                    </Link>
-                </div>
+        data-collapse="medium"
+        data-animation="default"
+        data-duration="400"
+        data-w-id="0b6d58b7-c8f6-c4c2-5d56-04a30b2a6532"
+        role="banner"
+        className="navigation no-color w-nav"
+      >
+        <div className="container grid w-container">
+          <div id="w-node-04a30b2a6534-0b2a6532" className="flex-center">
+            <Link passHref href="/" className="brand left-margin w-nav-brand">
+              <Image
+                width={150}
+                height={45}
+                alt="byecom logo"
+                src="/byecom-logo.png"
+              />
+            </Link>
+          </div>
 
-                <div id='w-node-04a30b2a6534-0b2a6533' className='flex-center'>
+          <div id="w-node-04a30b2a6534-0b2a6533" className="flex-center">
+            <nav role="navigation" className="nav-menu w-nav-menu">
+              <Link href="/career">
+                <a className="nav-link w-nav-link">Careers</a>
+              </Link>
+              <Link href="/contact">
+                <a className="nav-link w-nav-link">Contact</a>
+              </Link>
+            </nav>
+          </div>
 
-                    <nav role='navigation' className='nav-menu w-nav-menu'>
-                        
-                        <Link href='/career'>
-                            <a className='nav-link w-nav-link'>Careers</a>
-                        </Link>
-                        <Link href='/contact'>
-                            <a className='nav-link w-nav-link'>Contact</a>
-                        </Link>
-                        
-                    </nav>
-                </div>
-
-                <div id='w-node-04a30b2a6592-0b2a6532' className='flex-center'>
-                    <div className='menu-button w-nav-button'>
-                        <div className='line-1'>Hello</div>
-                        <div className='line-3'>Boy</div>
-                        <div className='line-2'></div>
-                    </div>
-                </div>
+          {/* <div id="w-node-04a30b2a6592-0b2a6532" className="flex-center">
+            <div className="menu-button w-nav-button">
+              <div className="line-1"></div>
+              <div className="line-3"></div>
+              <div className="line-2"></div>
             </div>
+          </div> */}
         </div>
-      <div className="sabya" style={{height: '5vh'}}>
+      </div>
+      <div className="sabya">
         <nav>
+          <div>
+            <Link passHref href="/" className="brand left-margin w-nav-brand">
+              <Image
+                width={150}
+                height={45}
+                alt="byecom logo"
+                src="/byecom-logo.png"
+              />
+            </Link>
+          </div>
           {(toggleMenu || screenWidth > 500) && (
             <ul className="list">
-              <li className="items">Home</li>
-              <li className="items">Services</li>
-              <li className="items">Contact</li>
+              <li className="items">
+                <Link href="/career" passHref>
+                  <a style={{ textDecoration: "none" }}>Careers</a>
+                </Link>
+              </li>
+
+              {/* <li className="items">Services</li> */}
+
+              <li className="items">
+                <Link href="/contact" passHref>
+                  <a style={{ textDecoration: "none" }}>Contact</a>
+                </Link>
+              </li>
             </ul>
           )}
-
-          <button onClick={toggleNav} className="btn">
-            BTN
-          </button>
+          <div>
+            <button onClick={toggleNav} className="btn">
+              <div className={!toggleMenu ? "container" : "change"}>
+                <div className="bar1"></div>
+                <div className="bar2"></div>
+                <div className="bar3"></div>
+              </div>
+            </button>
+          </div>
         </nav>
       </div>
     </>
