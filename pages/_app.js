26de2d9fe8c71d7ce55019818ab18d4867b8092c
Sabyasachi Rout <sabyasachirout@byecom.in>
2023-02-23 17:24:08 +0530

    nav font issue and allignment

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,20 @@
-import '../styles/landkit.webflow.scss';
-import '../styles/nav.scss'
-
+/* eslint-disable @next/next/no-page-custom-font */
+import "../styles/landkit.webflow.scss";
+import "../styles/nav.scss";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps}/>;
+  return (
+    <>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
